57df0437e24e5967cd9a040cea9e92457eec0152
wujiaming <289451937@qq.com>
2017-06-20 15:49:09 +0800

    init

--- src/main/java/com/jimmy/Application.java
+++ src/main/java/com/jimmy/Application.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 /**
  * Created by Jimmy on 2017/6/20.
  */
+
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) throws Exception {
