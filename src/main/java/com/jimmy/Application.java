771f896074b6970b190338a86c5eb204e5dabed7
wujiaming <289451937@qq.com>
2017-06-20 15:45:57 +0800

    init

---
+++ src/main/java/com/jimmy/Application.java
@@ -0,0 +1,14 @@
+package com.jimmy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by Jimmy on 2017/6/20.
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(Application.class, args);
+    }
+}
