815c0efe2a5162392905aaf153c98b1446bc5160
wujiaming <289451937@qq.com>
2017-06-21 11:33:32 +0800

    init

--- src/main/java/com/jimmy/configs/DubboBaseConfig.java
+++ src/main/java/com/jimmy/configs/DubboBaseConfig.java
@@ -6,34 +6,37 @@ import com.alibaba.dubbo.config.RegistryConfig;
 import com.jimmy.tools.SystemEnviroment;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ImportResource;
+import org.springframework.core.annotation.Order;
 
 /**
  * Created by Jimmy on 2017/6/20.
  */
 @Configuration
 //@PropertySource("classpath:dubbo/dubbo.properties")
-//@ImportResource("classpath:dubbo/*.xml")
+@ImportResource("classpath:dubbo/*.xml")
 public class DubboBaseConfig {
-    @Bean
-    public RegistryConfig registry() {
-        RegistryConfig registryConfig = new RegistryConfig();
-        registryConfig.setAddress(SystemEnviroment.getPropeties("dubbo.registry.address"));
-        registryConfig.setProtocol(SystemEnviroment.getPropeties("dubbo.registry.protocol"));
-        return registryConfig;
-    }
+//    @Bean
+//    public RegistryConfig registry() {
+//        RegistryConfig registryConfig = new RegistryConfig();
+//        registryConfig.setAddress(SystemEnviroment.getPropeties("dubbo.registry.address"));
+//        registryConfig.setProtocol(SystemEnviroment.getPropeties("dubbo.registry.protocol"));
+//        return registryConfig;
+//    }
+//
+//    @Bean
+//    public ApplicationConfig application() {
+//        ApplicationConfig applicationConfig = new ApplicationConfig();
+//        applicationConfig.setName(SystemEnviroment.getPropeties("dubbo.application.name"));
+//        return applicationConfig;
+//    }
+//
+//    @Bean
+//    public ProtocolConfig protocol() {
+//        ProtocolConfig protocolConfig = new ProtocolConfig();
+//        protocolConfig.setPort(SystemEnviroment.getIntPropeties("dubbo.protocol.port"));
+//        protocolConfig.setName(SystemEnviroment.getPropeties("dubbo.protocol.name"));
+//        return protocolConfig;
+//    }
 
-    @Bean
-    public ApplicationConfig application() {
-        ApplicationConfig applicationConfig = new ApplicationConfig();
-        applicationConfig.setName(SystemEnviroment.getPropeties("dubbo.application.name"));
-        return applicationConfig;
-    }
-
-    @Bean
-    public ProtocolConfig protocol() {
-        ProtocolConfig protocolConfig = new ProtocolConfig();
-        protocolConfig.setPort(SystemEnviroment.getIntPropeties("dubbo.protocol.port"));
-        protocolConfig.setName(SystemEnviroment.getPropeties("dubbo.protocol.name"));
-        return protocolConfig;
-    }
 }
--- src/main/java/com/jimmy/services/HelloServiceImpl.java
+++ src/main/java/com/jimmy/services/HelloServiceImpl.java
@@ -1,10 +1,12 @@
 package com.jimmy.services;
 
 import com.jimmy.api.HelloService;
+import org.springframework.stereotype.Service;
 
 /**
  * Created by Jimmy on 2017/6/20.
  */
+@Service
 public class HelloServiceImpl implements HelloService{
     @Override
     public String hello() {
